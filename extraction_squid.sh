d4b5602fa7b537bb5b04f930a40bb1dc00041650
Timothé Maammar <98331464+TimotheMaammar@users.noreply.github.com>
2022-04-02 10:15:29 +0000

    Update extraction_squid.sh

--- extraction_squid.sh
+++ extraction_squid.sh
@@ -4,4 +4,4 @@
 
 SITE=simat # Changer le nom du site ici
 
-cat access.log | grep $SITE | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u | more ### >> Résultat.txt
+cat /var/log/squid/access.log | grep $SITE | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u | more ### >> Résultat.txt
