5ecb7cd9932d6aeb7af97e397325b1d7c0c9974c
Timothé Maammar <98331464+TimotheMaammar@users.noreply.github.com>
2022-04-02 10:13:25 +0000

    Add files via upload

---
+++ extraction_squid.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+# Extraction des adresses IP qui se sont déjà connectées à un site spécifique à travers le Squid
+
+
+SITE=simat # Changer le nom du site ici
+
+cat access.log | grep $SITE | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u | more ### >> Résultat.txt
