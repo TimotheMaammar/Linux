68cff654b37dc487cab79b35aa58f2730eb6b92d
Timothé Maammar <98331464+TimotheMaammar@users.noreply.github.com>
2022-07-05 18:50:54 +0200

    Delete chrome.sh

--- chrome.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Installation de Google Chrome
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
