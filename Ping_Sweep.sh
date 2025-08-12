5f2eb6b9e75e394a866fc3969421e3d061606709
Timothé Maammar <98331464+TimotheMaammar@users.noreply.github.com>
2025-08-12 11:00:09 +0200

    Reconnaissance (dupliqué depuis /Payloads)

--- Ping_Sweep.sh
+++ Ping_Sweep.sh
@@ -2,6 +2,8 @@ for i in {1..254} ; do (ping -c 1 10.10.10.$i | grep "bytes from" &) ; done
 
 for i in {1..254} ; do for j in {1..254} ; do (ping -c 1 10.10.$i.$j | grep "bytes from" &) ; done ; done
 
+for i in $(cat wordlist.txt); do (ping -c 1 "$i.domaine.local" 2>/dev/null | grep "bytes from" &) ; done
+
 cat resultats.txt | awk '{print $4}' | tr -d ":"
 
 nmap -vv -iL ping_sweep.txt
